9fa0ef1c810c5d2b50c2c694752eba03068129b6
BereketeAb Birku <bereketeabbirku@gmail.com>
2024-08-24 20:09:15 +0300

    init

--- api/src/modules/iam/controllers/user.contoller.ts
+++ api/src/modules/iam/controllers/user.contoller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from '../services/user.service';
-import { SchemaCrudService } from 'src/utils/services/scheme-crud.service';
 import { UserDocument } from 'src/schemas/user.schema';
 import { SchemaCrudController } from 'src/utils/controllers/schema-crud.controller';
 
